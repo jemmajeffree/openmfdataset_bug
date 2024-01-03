64f5114d9e6edf4c16cd964e68f610ae7b708950
jemmajeffree <jemma.jeffree@anu.edu.au>
2024-01-02 20:22:08 -0700

    tidy up

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-import numpy as np
-
-for i in range(3):
-    data = xr.DataArray(np.random.normal(0,1,(100))) # Generate some data
-    data = data.expand_dims('dim1').assign_coords({'dim1':np.array((i,))}) # Make it so it can combine by coords easily
-    data.to_netcdf(str(i)+'.nc')
